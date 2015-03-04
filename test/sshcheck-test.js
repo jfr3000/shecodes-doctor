372edc0b1d5162d04c181a75f08454e946d74ff2
jfr3000 <julia.friesel@gmail.com>
2015-03-04 17:19:33 +0100

    added functionality and test for case of non-existing SSH keys.

--- test/sshcheck-test.js
+++ test/sshcheck-test.js
@@ -6,11 +6,17 @@ var expect = require('expect');
 
 var correctSSH = ['700', '600', '644'];
 var incorrectSSH = ['600', '700', '700'];
-lab.test('checkSSHSettings should work correctly', function(done) {
+lab.test('checkSSHSettings reacts to correct and incorrect existing SSH settings', function(done) {
     expect(checkSSHSettings(correctSSH)).toEqual([]);
     expect(checkSSHSettings(incorrectSSH)[0]).toMatch(/sudo chmod 700 .*\.ssh\//g);
     expect(checkSSHSettings(incorrectSSH)[1]).toMatch(/sudo chmod 600 .*\.ssh\/id_rsa/g);
     expect(checkSSHSettings(incorrectSSH)[2]).toMatch(/sudo chmod 644 .*\.ssh\/id_rsa.pub/g);
-    expect(checkSSHSettings(incorrectSSH)[2]).toMatch(/sudo chmod 644 .*\.ssh\/id_rsa.pub/g);
+    done();
+});
+
+lab.test('checkSSHSettings reacts correctly to non-existent SSH settings', function(done) {
+    expect(checkSSHSettings([])[0]).toMatch(/sudo chmod 700 .*\.ssh\//g);
+    expect(checkSSHSettings([])[1]).toMatch(/sudo chmod 600 .*\.ssh\/id_rsa/g);
+    expect(checkSSHSettings([])[2]).toMatch(/sudo chmod 644 .*\.ssh\/id_rsa.pub/g);
     done();
 });
